71b9e98829491a1a71775defef629b43236aeea1
Surjoyday Talukdar <surjoydaytalukdar.spn@outlook.com>
2024-09-05 03:18:03 +0530

    Functional Programming approach

--- functional.js
+++ functional.js
@@ -1,26 +1,52 @@
+const REQUIRED = "REQUIRED";
+const MIN_LENGTH = "MIN_LENGTH";
+
+function validate(value, flag, validatorValue) {
+  if (flag === REQUIRED) {
+    return value.trim().length > 0;
+  }
+
+  if (flag === MIN_LENGTH) {
+    return value.trim().length > validatorValue;
+  }
+}
+
+function getUserInput(inputElementID) {
+  return document.getElementById(inputElementID).value;
+}
+
+function createUser(userName, userPassword) {
+  if (!validate(userName, REQUIRED) || !validate(userPassword, MIN_LENGTH, 5)) {
+    throw new Error(
+      "Invalid input - username or password is wrong (password should be atleast 6 characters)"
+    );
+  }
+  return { user: userName, password: userPassword };
+}
+
+function greetUser(userObj) {
+  console.log(`H i am ${userObj.user}`);
+}
+
 function signUpHandler(e) {
   e.preventDefault();
 
-  const userNameInput = document.getElementById("user");
-  const enteredUserName = userNameInput.value;
+  const enteredUserName = getUserInput("user");
 
-  const passwordInput = document.getElementById("password");
-  const enteredPassword = passwordInput.value;
+  const enteredPassword = getUserInput("password");
 
-  if (enteredUserName.trim().length === 0) {
-    alert("Username field cannot be empty!");
-    return;
-  }
-
-  if (enteredPassword.trim().length <= 5) {
-    alert("Password must be at least 6 characters or longer!");
-    return;
+  try {
+    const newUser = createUser(enteredUserName, enteredPassword);
+    console.log(newUser);
+    greetUser(newUser);
+  } catch (err) {
+    alert(err.message);
   }
 }
 
-function connectForm(callback) {
-  const form = document.getElementById("user-input");
-  form.addEventListener("submit", callback);
+function connectForm(formID, formSubmitHandler) {
+  const form = document.getElementById(formID);
+  form.addEventListener("submit", formSubmitHandler);
 }
 
-connectForm(signUpHandler);
+connectForm("user-input", signUpHandler);
